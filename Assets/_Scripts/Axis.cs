dffd6a7483dbe471e3bf57dcf5ad6b6208364719
Grisha Revenko <grisha@revenko.org>
2022-08-28 11:54:53 +0300

    Added MouseY const

--- Assets/_Scripts/Axis.cs
+++ Assets/_Scripts/Axis.cs
@@ -5,4 +5,5 @@ public static class Axis
     public const string Horizontal = nameof(Horizontal);
     public const string Vertical = nameof(Vertical);
     public const string MouseX = "Mouse X";
+    public const string MouseY = "Mouse Y";
 }
